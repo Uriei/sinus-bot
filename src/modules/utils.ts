1747f98981e1f52c177f86207bf2e51370a7d143
Uriei <uriei@protonmail.com>
2025-05-07 00:19:27 +0200

    Added extra embed in case it goes further than 25 fields, will crash if people try to check too far in the future so the message is longer than 6k characters...

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,12 +1,14 @@
-import { APIEmbed, APIEmbedField, EmbedBuilder } from "discord.js";
+import { APIEmbed, APIEmbedField, Embed, EmbedBuilder } from "discord.js";
 import { IWeatherReport } from "./models/weather-report.model";
 
 const DEFAULT_COLOR = 0x033280;
 
 export function formatWeatherForecastForDiscord(weatherReport: IWeatherReport[], hours: number): Array<APIEmbed> {
-  const embed = new EmbedBuilder().setColor(DEFAULT_COLOR).setTitle(`Sinus Ardorum Weather Forecast for the next ${hours} hours`);
+  const embeds: Array<EmbedBuilder> = [];
+  const embedMain = new EmbedBuilder().setColor(DEFAULT_COLOR).setTitle(`Sinus Ardorum Weather Forecast for the next ${hours} hours`);
+  const fields = [];
 
-  for (let index = 0; index < weatherReport.length && index < 24; index++) {
+  for (let index = 0; index < weatherReport.length; index++) {
     const element = weatherReport[index];
     const nextElement = index < weatherReport.length - 1 ? weatherReport[index + 1] : null;
     let value = "";
@@ -26,11 +28,19 @@ export function formatWeatherForecastForDiscord(weatherReport: IWeatherReport[],
       value: value,
       inline: false,
     };
-
-    embed.addFields(field);
+    fields.push(field);
   }
+  embeds.push(embedMain);
 
-  return [embed.toJSON()];
+  for (let index = 0; index < fields.length; index++) {
+    if (embeds[embeds.length - 1].toJSON()?.fields?.length >= 25) {
+      embeds.push(new EmbedBuilder().setColor(DEFAULT_COLOR));
+    }
+    const field = fields.shift();
+    embeds[embeds.length - 1].addFields(field);
+  }
+  console.log(embeds);
+  return embeds.map((e) => e.toJSON());
 }
 
 export function wait(ms: number) {
